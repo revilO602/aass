40baa85ea7d5a5aff0b200d4ce5012878fca95e4
TomasLetanovsky <tomas.letanovsky@gmail.com>
2023-04-08 14:46:36 +0200

    Training selection submitting the chosen training

--- apps/aass-fe/src/screens/TrainingSelection.tsx
+++ apps/aass-fe/src/screens/TrainingSelection.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { redirect } from "react-router-dom";
+import React, { ChangeEvent } from "react";
+import { Navigate, redirect, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function TrainingSelection() {
-
-    //define traing type
-    type Training = {
+  //define traing type
+  type Training = {
     id: number;
     trainer_id: number;
     duration_min: number;
@@ -13,47 +12,67 @@ export default function TrainingSelection() {
     name: string;
     createdAt: string;
     updatedAt: string;
-}
+  };
 
-   
-    const [trainings, setTrainings] = useState<Training[]>([]);
+  const [trainings, setTrainings] = useState<Training[]>([]);
+  const [selectedTrainingId, setSelectedTrainingId] = useState<number | null>(null);
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedTrainingId(parseInt(event.target.value));
+  };
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch('http://localhost:8080/trainings/', {
-            method: "GET",
-            mode: "cors",
-            headers: { "Content-Type": "application/json" }
-        })
-        .then(response => {
-            console.log("API response received:", response);
-            return response.json();
-        })
-        .then(data => {
-            console.log("API data received:", data);
-            setTrainings(data);
-        })
-        .catch(error => {
-            console.error("API request failed:", error);
-        });
-    }, []);
-    
 
-        console.log(trainings);
 
-    return (   
-        <div>
-            <h1>Training Selection</h1>
-            <form>
-                <label>
-                    Training:
-                    <select>
-                        {trainings.map((training) => (
-                            <option value={training.id}>{training.name}</option>
-                        ))}
-                    </select>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
-        </div>
-    )
+  useEffect(() => {
+    fetch("http://localhost:8080/trainings/", {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        console.log("API response received:", response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("API data received:", data);
+        setTrainings(data);
+      })
+      .catch((error) => {
+        console.error("API request failed:", error);
+      });
+  }, []);
+
+  console.log(trainings);
+
+  const handleReserve = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/trainings/${selectedTrainingId}/reserve`
+      );
+      const data = await response.json();
+      console.log("API data received:", data);
+        navigate("/trainingConfirmation");
+    } catch (error) {
+      console.error("API request failed:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Training Selection</h1>
+      <div>
+        {/* Render a list of trainings with buttons to select them */}
+        <select value={selectedTrainingId || ""} onChange={handleChange}>
+          {trainings.map((training) => (
+            <option key={training.id} value={training.id}>
+              {training.name}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleReserve}>
+          Reserve
+        </button>
+      </div>
+    </div>
+  );
 }
